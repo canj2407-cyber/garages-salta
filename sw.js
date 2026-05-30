cc93dcdb91e32f412c87b0872f3c6e20d7d2557e
canj2407-cyber <canj2407@gmail.com>
2026-05-30 19:59:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE = 'garages-salta-v2';
-const URL_BASE = '/garages-salta/';
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll([URL_BASE, URL_BASE + 'index.html']))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match(URL_BASE + 'index.html')))
-  );
-});
+self.addEventListener('fetch', e => {});
